3d2f19b498628dd34dfac163239ba3f632f4b219
Marat <Shagiakhmetov2499@gmail.com>
2023-07-04 15:17:14 +0300

    Initial Commit

---
+++ MulticolorScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MulticolorScreen
+//
+//  Created by Marat Shagiakhmetov on 04.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
